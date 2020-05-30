fc22b5e5b0c348c65c57925753c6f60e40dfcc03
Stanisław Nagórski <stanislaw.nagorski@gmail.com>
2020-05-30 19:24:54 +0200

    styles added

--- src/main/java/org/AgendaCheck/ScheduleReader.java
+++ src/main/java/org/AgendaCheck/ScheduleReader.java
@@ -1,5 +1,9 @@
 package org.AgendaCheck;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -11,14 +15,28 @@ public class ScheduleReader {
     private XSSFSheet scheduleSheet;
     private XSSFWorkbook report;
     private XSSFSheet reportSheet;
+    private DataFormat dataFormat;
+    private CellStyle defaultFloatCellStyle;
+    private CellStyle boldedFloatWithTopBorder;
+    private XSSFFont boldFont;
     private List<Float> hoursSumByDay;
     private int scheduleRowsNumber;
+    private float monthlyHoursSum;
 
     public ScheduleReader(XSSFWorkbook schedule, XSSFWorkbook report) {
         this.schedule = schedule;
         this.scheduleSheet = schedule.getSheetAt(0);
         this.report = report;
         this.reportSheet = report.getSheetAt(0);
+        this.dataFormat = report.createDataFormat();
+        this.defaultFloatCellStyle = report.createCellStyle();
+        defaultFloatCellStyle.setDataFormat(dataFormat.getFormat("#.##"));
+        this.boldFont = report.createFont();
+        boldFont.setBold(true);
+        this.boldedFloatWithTopBorder = report.createCellStyle();
+        boldedFloatWithTopBorder.setDataFormat(dataFormat.getFormat("#.##"));
+        boldedFloatWithTopBorder.setFont(boldFont);
+        boldedFloatWithTopBorder.setBorderTop(BorderStyle.MEDIUM);
         this.hoursSumByDay = new ArrayList<>();
         this.scheduleRowsNumber = scheduleSheet.getLastRowNum();
     }
@@ -40,6 +58,7 @@ public class ScheduleReader {
                 daySum += scheduleSheet.getRow(i).getCell(j).getNumericCellValue();
             }
             hoursSumByDay.add(daySum);
+            monthlyHoursSum += daySum;
         }
     }
 
@@ -47,9 +66,13 @@ public class ScheduleReader {
 
         for (int i = 3; i < scheduleRowsNumber; i++) {
             reportSheet.getRow(i).createCell(1).setCellValue(hoursSumByDay.get(i - 3));
+            reportSheet.getRow(i).getCell(1).setCellStyle(defaultFloatCellStyle);
         }
 
         reportSheet.getRow(1).createCell(1).setCellValue("Suma godzin");
+        reportSheet.createRow(scheduleRowsNumber).createCell(1).setCellValue(monthlyHoursSum);
+        reportSheet.getRow(scheduleRowsNumber).getCell(1).setCellStyle(boldedFloatWithTopBorder);
+
         reportSheet.autoSizeColumn(1);
 
     }
