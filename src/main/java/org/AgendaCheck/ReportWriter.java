35d72222404b188b707977f25cad7900cab6bb79
Stanisław Nagórski <stahthecat@gmail.com>
2020-06-01 19:26:39 +0200

    class MonthChecker added, which check which month is to be reported. ReportWriter now write only forecasted Turn Over from desired month. added monthly TurnOver cell formats added

--- src/main/java/org/AgendaCheck/ReportWriter.java
+++ src/main/java/org/AgendaCheck/ReportWriter.java
@@ -9,33 +9,57 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.util.List;
 
 public class ReportWriter {
-    private ScheduleReader scheduleReader;
-    private ForecastReader forecastReader;
-    private XSSFSheet reportSheet;
-    private CellStyle defaultFloatCellStyle;
-    private CellStyle boldedFloatWithTopBorder;
-    private CellStyle percentageStyle;
+    private final ScheduleReader scheduleReader;
+    private final ForecastReader forecastReader;
+    private final XSSFSheet reportSheet;
+
+    private final CellStyle defaultFloatCellStyle;
+    private final CellStyle boldedFloatWithTopBorder;
+    private final CellStyle titleBoldedWithBotBorder;
+    private final CellStyle percentageStyle;
+    private final CellStyle polishZlotyStyle;
+    private final CellStyle polishZlotyStyleBoldedWithBotBorder;
+
+
+
 
     public ReportWriter(XSSFWorkbook report, ScheduleReader scheduleReader, ForecastReader forecastReader) {
         this.scheduleReader = scheduleReader;
         this.forecastReader = forecastReader;
 
         this.reportSheet = report.createSheet("Raport");
+
         DataFormat dataFormat = report.createDataFormat();
+        XSSFFont boldFont = report.createFont();
 
         this.defaultFloatCellStyle = report.createCellStyle();
         defaultFloatCellStyle.setDataFormat(dataFormat.getFormat("#.###"));
 
         this.boldedFloatWithTopBorder = report.createCellStyle();
         boldedFloatWithTopBorder.setDataFormat(dataFormat.getFormat("#.##"));
-        XSSFFont boldFont = report.createFont();
         boldFont.setBold(true);
         boldedFloatWithTopBorder.setFont(boldFont);
         boldedFloatWithTopBorder.setBorderTop(BorderStyle.MEDIUM);
         boldedFloatWithTopBorder.setAlignment(HorizontalAlignment.CENTER);
 
+        this.titleBoldedWithBotBorder = report.createCellStyle();
+        boldFont.setBold(true);
+        titleBoldedWithBotBorder.setFont(boldFont);
+        titleBoldedWithBotBorder.setBorderBottom(BorderStyle.MEDIUM);
+        titleBoldedWithBotBorder.setAlignment(HorizontalAlignment.CENTER);
+
         this.percentageStyle = report.createCellStyle();
         percentageStyle.setDataFormat(dataFormat.getFormat("0.00%"));
+
+        this.polishZlotyStyle = report.createCellStyle();
+        polishZlotyStyle.setDataFormat(dataFormat.getFormat("###0,00\\ \"zł\";-###0,00\\ \"zł\""));
+
+        this.polishZlotyStyleBoldedWithBotBorder = report.createCellStyle();
+        polishZlotyStyleBoldedWithBotBorder.setDataFormat(dataFormat.getFormat("###0,00\\ \"zł\";-###0,00\\ \"zł\""));
+        boldFont.setBold(true);
+        polishZlotyStyleBoldedWithBotBorder.setFont(boldFont);
+        polishZlotyStyleBoldedWithBotBorder.setBorderTop(BorderStyle.MEDIUM);
+        polishZlotyStyleBoldedWithBotBorder.setAlignment(HorizontalAlignment.CENTER);
     }
 
     public void writeFirstRowFromCopy() {
@@ -45,6 +69,10 @@ public class ReportWriter {
             reportSheet.createRow(i).createCell(0)
                     .setCellValue(scheduleReader.getFirstColumn().get(i));
         }
+
+        XSSFCell titleOfDateColumn = reportSheet.getRow(1).getCell(0);
+        titleOfDateColumn.setCellStyle(titleBoldedWithBotBorder);
+
         reportSheet.autoSizeColumn(0);
     }
 
@@ -62,8 +90,13 @@ public class ReportWriter {
 
         }
 
-        reportSheet.getRow(1).createCell(1).setCellValue("Suma godzin");
-        reportSheet.getRow(1).createCell(2).setCellValue("Udział dnia");
+        XSSFCell titleOfHoursColumn = reportSheet.getRow(1).createCell(1);
+        titleOfHoursColumn.setCellValue("Suma godzin");
+        titleOfHoursColumn.setCellStyle(titleBoldedWithBotBorder);
+
+        XSSFCell titleOfDayShareInHoursColumn = reportSheet.getRow(1).createCell(2);
+        titleOfDayShareInHoursColumn.setCellValue("Udział dnia w godzinach");
+        titleOfDayShareInHoursColumn.setCellStyle(titleBoldedWithBotBorder);
 
         reportSheet.createRow(scheduleReader.getScheduleRowsNumber()).createCell(1)
                 .setCellValue(scheduleReader.getMonthlyHoursSum());
@@ -84,15 +117,20 @@ public class ReportWriter {
 
         List<Double> forecast = forecastReader.forecastTOList(range);
 
-        reportSheet.getRow(1).createCell(3).setCellValue("Pilotaż obrotu");
+        XSSFCell titleOfTurnOverColumn = reportSheet.getRow(1).createCell(3);
+        titleOfTurnOverColumn.setCellValue("Pilotaż obrotu");
+        titleOfTurnOverColumn.setCellStyle(titleBoldedWithBotBorder);
 
         for (int i = 0; i < forecast.size(); i++) {
 
             XSSFCell cell = reportSheet.getRow(i + 3).createCell(3);
             cell.setCellValue(forecast.get(i));
-            cell.setCellStyle(defaultFloatCellStyle);
-
+            cell.setCellStyle(polishZlotyStyle);
         }
+
+        reportSheet.getRow(forecast.size()+2)
+                .getCell(3).setCellStyle(polishZlotyStyleBoldedWithBotBorder);
+
         reportSheet.autoSizeColumn(3);
 
     }
